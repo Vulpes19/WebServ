d93bd098464f20af8c0bee0a6c80d685d0aca705
0xDopamine <aminebumi1@gmail.com>
2023-06-13 21:24:27 +0100

    trying with the Context class, figuring out how to parse nested directives

--- srcs/include/parser.hpp
+++ srcs/include/parser.hpp
@@ -6,7 +6,7 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 18:43:59 by mbaioumy          #+#    #+#             */
-/*   Updated: 2023/06/12 20:18:44 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2023/06/13 21:21:12 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,18 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <stack>
 #include <sstream>
 #include <algorithm>
 
+class Context {
+    public:
+        std::string type;
+        std::map<std::string, std::string>  block;
+    private:
+        Context(std::string& type);
+        ~Context();
+
+} ;
 
 #endif
--- srcs/parser.cpp
+++ srcs/parser.cpp
@@ -6,15 +6,21 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 18:42:15 by mbaioumy          #+#    #+#             */
-/*   Updated: 2023/06/12 20:33:42 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2023/06/13 21:22:34 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/parser.hpp"
 
+Context::Context(std::string& type): type(type) {};
+
+Context::~Context() {};
+
 int main(int argc, char **argv) {
 
-    std::ifstream   confFile(argv[1]);
+    std::ifstream       confFile(argv[1]);
+    std::stack<Context> nestedDirs;
+    Context cont;
     if (confFile.is_open())
     {
         std::string line;
@@ -22,11 +28,13 @@ int main(int argc, char **argv) {
             if (line[0] == '#' || line.empty())
                 continue ;
             std::stringstream ss(line);
-            std::string directive;
-            std::string value;
-            std::getline(ss, directive, ' ');
-            std::cout << "directive: " << directive << std::endl;
-            std::cout << "value: " << value << std::endl;
+            std::string directive, value;
+            ss >> directive >> value;
+            if (value[0] == '{')
+            {
+                cont.type = directive;
+                cont.block[]
+            }
         }
     }
     return 0;
