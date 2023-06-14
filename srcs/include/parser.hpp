cb12fd8fde7f0becc5e93b228ce3c1cbd3d6160a
0xDopamine <aminebumi1@gmail.com>
2023-06-14 15:16:21 +0100

    trying with data structs

--- srcs/include/parser.hpp
+++ srcs/include/parser.hpp
@@ -6,7 +6,7 @@
 /*   By: mbaioumy <mbaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 18:43:59 by mbaioumy          #+#    #+#             */
-/*   Updated: 2023/06/13 21:21:12 by mbaioumy         ###   ########.fr       */
+/*   Updated: 2023/06/14 15:14:40 by mbaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,32 @@
 #include <sstream>
 #include <algorithm>
 
-class Context {
-    public:
-        std::string type;
-        std::map<std::string, std::string>  block;
+
+class Location {
     private:
-        Context(std::string& type);
+        std::string value;
+        std::string root;
+        std::string index;
+    public:
+        Location(std::string value, std::string root, std::string index);
+        ~Location();
+}
+
+class Server {
+    private:
+        std::string             port;
+        std::string             server_name;
+        std::stack<Location>    locations;
+    public:
+        Server(std::string port, std::string server_name);
+        ~Server();
+}
+
+class Context {
+    private:
+        Server  server;
+    public:
+        Context(Server server);
         ~Context();
-
 } ;
-
 #endif
